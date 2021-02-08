bccfd674cd9bbe23d208fb15a76633de309e4de1
jwilson42 <wilson.jd022@gmail.com>
2021-02-08 15:07:24 -0800

    added more information on controllers

--- controllers/users.js
+++ controllers/users.js
@@ -19,8 +19,27 @@ router.get('/:id', (req, res) => {
         },
         include: [db.pet]
     }).then( user => {
-        console.log(user.pets);
+        console.log(user);
+        res.render('./userShow', { user });
+    }).catch(error => {
+        console.log('An error has occured when getting a user.');
+        res.redirect('/users');
     });
-})
+});
+
+// Add post route
+router.post('/:id/newPet', (req, res) => {
+    db.pet.create({
+        name: req.body.name,
+        species: req.body.species,
+        description: req.body.description,
+        userId: req.params.id,
+    }).then(newPet => {
+        res.redirect(`/users/${req.params.id}`);
+    }).catch(error => {
+        console.log('error in creating a new pet');
+        console.log(error);
+    });
+});
 
 module.exports = router;
